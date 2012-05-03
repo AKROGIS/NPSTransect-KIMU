774f3d604d579fec068619e8def2ccd8a476f614
Regan Sarwas <regan_sarwas@nps.gov>
2012-05-03 08:56:50 -0800

    Fixed Issue B.7: re-ordered behavior and groupsize on data grid, and enabled edit mode with keystroke in grid cell.

--- AnimalObservations/EditObservationAttributesPage.xaml.cs
+++ AnimalObservations/EditObservationAttributesPage.xaml.cs
@@ -6,7 +6,10 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
 using ESRI.ArcGIS.Mobile.Client;
+using Microsoft.Windows.Controls;
 
 namespace AnimalObservations
 {
@@ -203,6 +206,88 @@ namespace AnimalObservations
             dataGrid.CommitEdit();
         }
 
+        #endregion
+
+
+
+        #region fast edit of data grid cells
+
+        // Pushes combo boxes (and checkboxes) into edit mode on first click/keystroke, instead of standard two click
+        // From http://stackoverflow.com/questions/3833536/how-to-perform-single-click-checkbox-selection-in-wpf-datagrid
+
+        private void DataGridCell_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DataGridCell cell = sender as DataGridCell;
+            GridColumnFastEdit(cell, e);
+        }
+
+        private void DataGridCell_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            DataGridCell cell = sender as DataGridCell;
+            GridColumnFastEdit(cell, e);
+        }
+
+        private static void GridColumnFastEdit(DataGridCell cell, RoutedEventArgs e)
+        {
+            if (cell == null || cell.IsEditing || cell.IsReadOnly)
+                return;
+
+            DataGrid dataGrid = FindVisualParent<DataGrid>(cell);
+            if (dataGrid == null)
+                return;
+
+            if (!cell.IsFocused)
+            {
+                cell.Focus();
+            }
+
+            if (cell.Content is CheckBox)
+            {
+                if (dataGrid.SelectionUnit != DataGridSelectionUnit.FullRow)
+                {
+                    if (!cell.IsSelected)
+                        cell.IsSelected = true;
+                }
+                else
+                {
+                    DataGridRow row = FindVisualParent<DataGridRow>(cell);
+                    if (row != null && !row.IsSelected)
+                    {
+                        row.IsSelected = true;
+                    }
+                }
+            }
+            else
+            {
+                ComboBox cb = cell.Content as ComboBox;
+                if (cb != null)
+                {
+                    //DataGrid dataGrid = FindVisualParent<DataGrid>(cell);
+                    dataGrid.BeginEdit(e);
+                    cell.Dispatcher.Invoke(
+                     DispatcherPriority.Background,
+                     new Action(delegate { }));
+                    cb.IsDropDownOpen = true;
+                }
+            }
+        }
+
+
+        private static T FindVisualParent<T>(UIElement element) where T : UIElement
+        {
+            UIElement parent = element;
+            while (parent != null)
+            {
+                T correctlyTyped = parent as T;
+                if (correctlyTyped != null)
+                {
+                    return correctlyTyped;
+                }
+
+                parent = VisualTreeHelper.GetParent(parent) as UIElement;
+            }
+            return null;
+        }
         #endregion
     }
 }
